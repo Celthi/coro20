f7f4917bb9f375cade2795572d435701be524113
evan <evan_l00@qq.com>
2024-02-25 20:20:47 +0800

    add handler wrapper

--- src/coro_fizz.cpp
+++ src/coro_fizz.cpp
@@ -3,21 +3,26 @@
 #include <optional>
 #include <source_location>
 #include <fstream>
+#include <type_traits>
 class PlantUML
 {
-    private:
+private:
     std::string m_file_name;
     std::ofstream m_file;
 
 public:
-    PlantUML(std::string file_name = "coro_fizz.puml") : m_file_name(file_name) {
+    PlantUML(std::string file_name = "coro_fizz.puml") : m_file_name(std::move(file_name))
+    {
         m_file.open(m_file_name);
-        if (!m_file.is_open()) {
+        if (!m_file.is_open())
+        {
             std::cerr << "Failed to open file " << m_file_name << std::endl;
         }
     }
-    ~PlantUML() {
-        if (m_file.is_open()) {
+    ~PlantUML()
+    {
+        if (m_file.is_open())
+        {
             m_file.close();
         }
     }
@@ -25,7 +30,8 @@ public:
     PlantUML &operator=(const PlantUML &) = delete;
     PlantUML(PlantUML &&) = delete;
     PlantUML &operator=(PlantUML &&) = delete;
-    static PlantUML& get_instance() {
+    static PlantUML &get_instance()
+    {
         static PlantUML instance;
         return instance;
     }
@@ -45,8 +51,8 @@ public:
                   << note << " \nend note\n"
                   << std::endl;
         m_file << "note right\n"
-                << note << " \nend note\n"
-                << std::endl;
+               << note << " \nend note\n"
+               << std::endl;
     }
 
     void note_over(const std::string &note, const std::string &participant)
@@ -55,8 +61,8 @@ public:
                   << note << " \nend note\n"
                   << std::endl;
         m_file << "note over " << participant << "\n"
-                << note << " \nend note\n"
-                << std::endl;
+               << note << " \nend note\n"
+               << std::endl;
     }
 
     void add_participant(const std::string &participant)
@@ -76,19 +82,77 @@ public:
     }
 };
 
+// class to wrap the coroutine handle to mark status transition
+template <typename P>
+class CoroHandler
+{
+public:
+    std::string m_name;
+    std::coroutine_handle<P> handle;
+    std::vector<std::string> m_transfer_targets;
+    CoroHandler(std::string name, std::coroutine_handle<P> h) : m_name(std::move(name)), handle(h) {}
+    CoroHandler(const CoroHandler &) = default;
+    CoroHandler &operator=(const CoroHandler &) = default;
+    CoroHandler(CoroHandler &&s) = default;
+    CoroHandler &operator=(CoroHandler &&s) = default;
+
+    void suspend(std::string target)
+    {
+        PlantUML::get_instance().message(m_name, target, "suspend()");
+        m_transfer_targets.push_back(target);
+    }
+
+    void resume()
+    {
+        PlantUML::get_instance().message(m_transfer_targets.back(), m_name, "resume()");
+        handle.resume();
+    }
+
+    bool done()
+    {
+        return handle.done();
+    }
+
+    void destroy()
+    {
+        if (handle)
+            handle.destroy();
+    }
+
+    explicit operator bool() const
+    {
+        return handle != nullptr;
+    }
+
+    std::conditional_t<std::is_void_v<P>, std::coroutine_handle<>, std::coroutine_handle<P>> get_handle(const std::string &from, const std::string &msg)
+    {
+        PlantUML::get_instance().message(from, m_name, msg);
+        return handle;
+    }
+
+    // if P is not void then define promise method
+    template <typename T = P>
+    std::enable_if_t<!std::is_void_v<T>, std::add_lvalue_reference_t<T>> promise()
+    {
+        return handle.promise();
+    }
+};
+
 struct YieldAwaitable
 {
-    std::coroutine_handle<> consumer_coro_handle;
-    YieldAwaitable() : consumer_coro_handle(nullptr) {}
-    explicit YieldAwaitable(std::coroutine_handle<> h) : consumer_coro_handle(h) {}
+    CoroHandler<void> consumer_coro_handle;
+    std::string m_name;
+    YieldAwaitable() : consumer_coro_handle("null", nullptr)
+    {
+        m_name = "YieldAwaitable";
+    }
+    explicit YieldAwaitable(std::coroutine_handle<> h) : consumer_coro_handle("consume_numbers", h) {}
     constexpr bool await_ready() const noexcept { return false; }
     std::coroutine_handle<> await_suspend(std::coroutine_handle<>) noexcept
     {
-        PlantUML::get_instance().message("YieldAwaitable", "consume_numbers", "consumer_coro_handle.resume()");
         if (consumer_coro_handle)
         {
-            PlantUML::get_instance().message("consume_numbers", "GenNumberAwaiter", "co_await");
-            return consumer_coro_handle;
+            return consumer_coro_handle.get_handle(m_name, "await_suspend");
         }
         return std::noop_coroutine();
     }
@@ -105,46 +169,49 @@ class GenNumber
 public:
     struct promise_type;
     using handle_type = std::coroutine_handle<promise_type>;
-    handle_type handle; // the coroutine handle
+    CoroHandler<promise_type> handle; // the coroutine handle
     // rule of zero
-    GenNumber() = default;
-    GenNumber(const GenNumber &) = delete;            // 1. no copy constructor
-    GenNumber &operator=(const GenNumber &) = delete; // 2. no copy assignment
-    explicit GenNumber(handle_type h) : handle(h) {}  // 3. constructor
-    ~GenNumber()                                      // 4. destructor
+    GenNumber() = delete;
+    GenNumber(const GenNumber &) = delete;                        // 1. no copy constructor
+    GenNumber &operator=(const GenNumber &) = delete;             // 2. no copy assignment
+    explicit GenNumber(handle_type h) : handle("GenNumber", h) {} // 3. constructor
+    ~GenNumber()                                                  // 4. destructor
     {
         if (handle)
         {
             handle.destroy();
         }
     }
-    GenNumber(GenNumber &&s) : handle(s.handle) // 5. move constructor
+    GenNumber(GenNumber &&s) : handle("GenNumber", s.handle.handle) // 5. move constructor
     {
-        s.handle = nullptr;
+        s.handle.handle = nullptr;
     }
     GenNumber &operator=(GenNumber &&s) // 6. move assignment
     {
-        handle = s.handle;
-        s.handle = nullptr;
+        handle = std::move(s.handle);
         return *this;
     }
 
     class GenNumberAwaiter
     {
     public:
-        handle_type producer_handler;
-        explicit GenNumberAwaiter(handle_type p) : producer_handler(p) {}
-
+        CoroHandler<promise_type> producer_handler;
+        explicit GenNumberAwaiter(const CoroHandler<promise_type> &p) : producer_handler("generate_numbers", p.handle) {}
+        GenNumberAwaiter(const GenNumberAwaiter &) = default;
+        GenNumberAwaiter &operator=(const GenNumberAwaiter &) = default;
+        GenNumberAwaiter(GenNumberAwaiter &&) = default;
+        GenNumberAwaiter &operator=(GenNumberAwaiter &&) = default;
         bool await_ready() const { return false; }
-        std::coroutine_handle<> await_suspend(std::coroutine_handle<>) const
+        std::coroutine_handle<promise_type> await_suspend(std::coroutine_handle<>)
         {
             PlantUML::get_instance().note_over("GenNumberAwaiter::await_suspend", "GenNumberAwaiter");
             PlantUML::get_instance().message("GenNumberAwaiter", "generate_numbers", "producer_handler.resume()");
-            return producer_handler;
+            return producer_handler.get_handle("GenNumberAwaiter", "await_suspend");
         }
 
         std::optional<Value> await_resume()
         {
+            PlantUML::get_instance().message("generate_numbers", "GenNumberAwaiter", "await_resume");
             PlantUML::get_instance().message("GenNumberAwaiter", "consume_numbers", "await_resume");
             return producer_handler.promise().value;
         }
@@ -170,6 +237,7 @@ public:
         // the co_yield expression will call this function
         YieldAwaitable yield_value(Value v)
         {
+            
             PlantUML::get_instance().message("generate_numbers", "YieldAwaitable", "yield_value");
             value = v;
             return YieldAwaitable{consumer_coro_handle};
@@ -186,9 +254,9 @@ public:
     handle_type handle; // Consumer coroutine handle
     struct promise_type
     {
-        std::optional<Value> value;                                     // the value to be returned
-        std::coroutine_handle<GenNumber::promise_type> producer_handle; // the producer coroutine handle
-        promise_type(const GenNumber &source, int divisor) : producer_handle(source.handle) {}
+        std::optional<Value> value;                             // the value to be returned
+        CoroHandler<GenNumber::promise_type> &producer_handler; // the producer coroutine handle
+        promise_type(GenNumber &source, int divisor) : producer_handler(source.handle) {}
         promise_type(const promise_type &) = delete;
         promise_type &operator=(const promise_type &) = delete;
         Consumer get_return_object()
@@ -200,14 +268,14 @@ public:
         void return_void() const {}
         void unhandled_exception() const {}
         // await_transform method
-        GenNumber::GenNumberAwaiter await_transform(const GenNumber &source)
+        GenNumber::GenNumberAwaiter await_transform(GenNumber &source)
         {
-
             PlantUML::get_instance().message("consume_numbers", "GenNumberAwaiter", "await_transform");
             auto awaitable = GenNumber::GenNumberAwaiter{source.handle};
             source.handle.promise().consumer_coro_handle = std::coroutine_handle<promise_type>::from_promise(*this);
             return awaitable;
         }
+
         std::suspend_always yield_value(std::optional<Value> v)
         {
             value = v;
@@ -241,10 +309,11 @@ public:
         {
             return {};
         }
-        handle.promise().producer_handle.promise().value = {};
+        handle.promise().producer_handler.promise().value = {};
         handle.resume();
-        auto v = handle.promise().producer_handle.promise().value;
+        auto v = handle.promise().producer_handler.promise().value;
         return v;
+        return {};
     }
     bool done()
     {
@@ -268,6 +337,8 @@ Consumer consume_numbers(GenNumber source, int divisor)
     PlantUML::get_instance().note_over("consume_numbers is about to call await_transform", "consume_numbers");
     while (std::optional<Value> vopt = co_await source) // Consumer::await_transform -> GenNumberAwaiter::await_suspend ->generate_numbers::resume() -> *yieldawaitable::await_suspend* -> consumer_coro_handle.resume() -> GenNumberAwaiter::await_resume
     {
+        PlantUML::get_instance().message("consume_numbers", "GenNumberAwaiter", "co_await");
+
         PlantUML::get_instance().note_over("consume_numbers co_await result = " + std::to_string(*vopt), "consume_numbers");
         if (*vopt % divisor == 0)
         {
